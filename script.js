9aaa15ce47bb055dd058770e76825737233e4b20
Jesse Willig <jessewillig@outlook.com>
2021-02-08 14:54:51 -0800

    ..

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
-// global variable for user name
-var userInput = [];
-
-// push user input into userInput array
-function pushInput() {
-    var userName = $("#userName").val();
-    userInput.push(userName);
-    storeUser();
-    getUser();
-};
-
-// store username in local storage
-// user input field
-$("#userName").on("click", function (event) {
+// Click handler for localStorage and page redirect
+$("#submitBtn").on("click", function (event) {
     event.preventDefault();
     var userName = $("#userName").val();
-    localStorage.setItem("user", JSON.stringify(userName));   
-
+    localStorage.setItem("user", JSON.stringify(userName));
+    document.location.href = "index2.html"
 });
-    var getUser = JSON.parse(localStorage.getItem("user"));
-    var welcomeBanner = $("<p>").addClass("title").text("Welcome, " + getUser);
-    $("#userName").append(welcomeBanner)
-    
+// Retrieve user name from localStorage and display in hero
+var getUser = JSON.parse(localStorage.getItem("user"));
+var welcomeBanner = $("<p>").addClass("title").text("Welcome, " + getUser);
+$("#userName").append(welcomeBanner)
 
-// API function
+
+// API functions
 $(document).ready(function () {
-    // attach button click event listener to Kanye function
+    //Kanye API
+    //Show Kanye modal when Kanye button is clicked
     $(".kanyeBtn").click(function () {
-        // creating modal to show quote
-        $(".modal").addClass("is-active");
+        $("#KanyeModal").addClass("is-active");
         kanyeQuotes();
     });
-    $(".modal-close").click(function () {
-        $(".modal").removeClass("is-active");
+    //Hide Kanye modal when restart button is clicked
+    $("#KanyeRestartBtn").click(function () {
+        $("#KanyeModal").removeClass("is-active");
     });
-    $("#restartBtn").click(function () {
-        $(".modal").removeClass("is-active");
-    });
+    //Get data from Kanye API and display in modal
     function kanyeQuotes() {
         var kanyeUrl = `https://api.kanye.rest?format=text`;
 
@@ -45,12 +33,13 @@ $(document).ready(function () {
             dataType: "text",
         }).then(function (response) {
             console.log(response);
-            $(".quote").empty();
-            $(".quote").append(response);
+            $("#KanyeQuote").empty();
+            $("#KanyeQuote").append(response);
         });
     };
 
     //NASA API
+    //Get data from NASA API and display in modal
     function getNASAData() {
         var NASAURL = "https://api.nasa.gov/planetary/apod?api_key=cTfEOxMXJxJDzjrpL2IhFf1wseJMQ660LAtJwmWy";
         $.ajax({
@@ -69,29 +58,75 @@ $(document).ready(function () {
 
     //Show NASA modal when click NASA button
     $("#NASAbtn").click(function () {
-        $(".NASAModal").addClass("is-active");
+        $("#NASAModal").addClass("is-active");
         getNASAData();
     });
 
     //Hide NASA modal when click Restart button
     $("#NASArestartBtn").click(function () {
-        $(".NASAModal").removeClass("is-active");
+        $("#NASAModal").removeClass("is-active");
     });
 
     //Joke API function
-    function Jokes() {
-        var JokesUrl = `https://v2.jokeapi.dev/joke/Programming,Miscellaneous?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&format=txt`;
+    //Show joke modal when click Joke button
+     $("#jokeBtn").click(function () {
+        $("#jokeModal").addClass("is-active");
+        //gathers data and displays joke
+        jokes();
+    });
+    //Hide joke modal when click restart button
+    $("#jokeRestartBtn").click(function () {
+        $("#jokeModal").removeClass("is-active");
+    });
+    //Gather data and display joke
+    function jokes() {
+        var jokeUrl = `https://v2.jokeapi.dev/joke/Programming,Miscellaneous?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&format=txt`;
+
         $.ajax({
-            url: JokesUrl,
-            method: "GET"
+            url: jokeUrl,
+            method: "GET",
+            dataType: "text",
         }).then(function (response) {
-            return response.JSON();
-        }).then(function (data) {
-            console.log(data);
+            console.log(response);
+            $("#joke").empty();
+            $("#joke").append(response);
         });
-
-
+    };
+    
+    //Panorama API
+    function initialize() {
+        var Venice = { lat: 45.4759903, lng: 12.5915585 }
+        var Seychelles = { lat: -4.3325215, lng: 55.7870521 }
+        var Boudha = { lat: 27.721378, lng: 85.3619399 }
+        var Seoraksan = { lat: 38.0803894, lng: 128.4360266 }
+        var Mahakai = { lat: 27.0477084, lng: 88.2678402 }
+        var Baikal = { lat: 53.3255194, lng: 107.7484936 }
+        var Tbilisi = { lat: 41.6870833, lng: 44.808745 }
+        var Baalbek = { lat: 34.0045188, lng: 36.2030793 }
+        var Polonnaruwa = { lat: 7.9473986, lng: 81.0013532 }
+        var locations = [Venice, Seychelles, Boudha, Seoraksan, Mahakai, 
+            Baikal, Tbilisi, Baalbek, Polonnaruwa]
+        randomPlace = locations[Math.floor(Math.random() * locations.length)]
+        new google.maps.StreetViewPanorama(
+        document.getElementById("streetviewPanorama"),
+        {
+            position:randomPlace,
+            pov: { heading: 0, pitch: 0 },
+            zoom: 1,
+        },
+        )
+        console.log(locations)
+        console.log(randomPlace)
     }
-    Jokes();
+    //Click to open Panorama Modal
+    $("#travel").click(function () {
+        $(".panoramaModal").addClass("is-active"),
+        initialize ()
+    });
+    //Click to close Panorama Modal 
+    $("#panoramarestartBtn").click(function () {
+        $(".panoramaModal").removeClass("is-active"),
+        document.getElementById("streetviewPanorama").innerHTML = "";
+    });
 
 });
